2c95f3ff4faf6eaebe54d76c6d91767de422c082
Denisov Egor <egordenisov1505@mail.ru>
2021-09-15 01:21:57 +0300

    Initial Commit

---
+++ Interpolation polynomials/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Interpolation polynomials
+//
+//  Created by Egor Denisov on 15.09.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
